f5d0e7ab1d35c1a254d02e1640764f461e8302da
BigtoC <taitocdt@gmail.com>
2023-03-15 23:27:21 +0800

    feat(config): update environment setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,51 +3,38 @@ require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-etherscan");
-require('hardhat-docgen');
+require("hardhat-docgen");
+
+const { ALCHEMY_API_KEY, DEV_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-    },
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_ROPSTEN_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      timeout: 600000,
-    },
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${DEV_PRIVATE_KEY}`],
       gasPrice: 2500000000,
     },
     mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${DEV_PRIVATE_KEY}`],
       timeout: 60000,
       // gasPrice: 75000000000,
     },
-    palm: {
-      url: `https://palm-mainnet.infura.io/v3/${process.env.PALM_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-    },
-    arbitrum: {
-      url: "https://arb1.arbitrum.io/rpc",
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      timeout: 600000,
-    },
-    frame: {
-      url: "http://127.0.0.1:1248",
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${DEV_PRIVATE_KEY}`],
+      timeout: 60000,
+      gasPrice: 2500000000,
     },
     hardhat: {
       mining: {
         auto: true,
       },
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
         blockNumber: 12772572,
       },
     },
@@ -65,6 +52,6 @@ module.exports = {
     timeout: 100000,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: ETHERSCAN_API_KEY
   },
 };
